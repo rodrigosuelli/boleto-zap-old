80bca08ee8b48443e08e61cc24b9a86014858d1b
rodrigosuelli <rodrigosuelli@gmail.com>
2022-05-15 10:40:40 -0300

    Fix currentDatePlus func

--- utils/dates.js
+++ utils/dates.js
@@ -1,12 +1,12 @@
 const OneDayInMilliseconds = 86400000;
 
 // GMT -03:00 (Brasilia Standard Time)
-const currentDate = new Date(new Date() - 3600 * 1000 * 3 + 1000)
-  .toISOString()
-  .split('T')[0];
+const date = new Date(new Date() - 3600 * 1000 * 3 + 1000);
+
+const currentDate = date.toISOString().split('T')[0];
 
 function currentDatePlus(days) {
-  return new Date(currentDate.getTime() + OneDayInMilliseconds * days)
+  return new Date(date.getTime() + OneDayInMilliseconds * days)
     .toISOString()
     .split('T')[0];
 }
